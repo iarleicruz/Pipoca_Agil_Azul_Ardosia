2d1a2190d3579d96246062b954b81dd6d6339da3
iarleicruz <iarlei.souza@gmail.com>
2025-05-14 11:31:02 -0300

    feat: teste

--- contagem.js
+++
@@ -1,9 +0,0 @@
-let visitantes = []
-
-function contagem(){
-    for(let cont = 0; cont < visitantes.length; cont++){
-        visitantes += cont
-        console.log(visitantes)
-    }
-}
-console.log(contagem())
